0c9dd2054b548c75e7875860ad41d379e78e7dde
Jonyan_Dunh <1309634881@qq.com>
2020-07-30 16:21:54 +0800

    16点21分

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Bilibili_Client
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;//显示位置屏幕居中
+        }
+        private void Window_MouseMove(object sender, MouseEventArgs e)//移动窗口
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void index_button(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
