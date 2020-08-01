a4f0e2ba500abaaeedbb5dbccf44b5e86421f52b
Jonyan_Dunh <1309634881@qq.com>
2020-08-01 23:08:52 +0800

    23点08分

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
@@ -34,6 +35,18 @@ namespace Bilibili_Client
                 //创建DATA文件夹
                 Directory.CreateDirectory(@"Data\Cache");
             }
+            List<left_button_class> Left_button_class = new List<left_button_class>();
+            Left_button_class.Add(new left_button_class("resource/img/首页.png", "首页"));
+            Left_button_class.Add(new left_button_class("resource/img/动态.png", "动态"));
+            Left_button_class.Add(new left_button_class("resource/img/排行.png", "排行"));
+            Left_button_class.Add(new left_button_class("resource/img/分类.png", "分类"));
+            Left_button_class.Add(new left_button_class("resource/img/直播.png", "直播"));
+            Left_button_class.Add(new left_button_class("resource/img/番剧.png", "番剧"));
+            Left_button_class.Add(new left_button_class("resource/img/漫画.png", "漫画"));
+            Left_button_class.Add(new left_button_class("resource/img/会员购.png", "会员购"));
+            Left_button_class.Add(new left_button_class("resource/img/游戏中心.png", "游戏中心"));
+            Left_button_class.Add(new left_button_class("resource/img/投稿工具.png", "投稿工具"));
+            left_button_box.ItemsSource = Left_button_class;
         }
 
         private void Window_MouseMove(object sender, MouseEventArgs e)//移动窗口
@@ -56,6 +69,23 @@ namespace Bilibili_Client
 
         }
 
+        public class left_button_class
+        {
+            public string left_button_img { get; private set; }
+            public string left_button_name { get; private set; }
+
+
+
+            public left_button_class(
+                string button_img, 
+                string button_name
+                )
+            {
+                left_button_img = button_img;
+                left_button_name = button_name;
+
+            }
+        }
 
     }
 }
